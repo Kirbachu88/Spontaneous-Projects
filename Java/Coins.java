35323b4a2a5036f6f0b52a254361b97ccf34923b
Angela <69411622+Kirbachu88@users.noreply.github.com>
2020-09-17 20:55:29 -0700

    Another attempt at change counter

--- Java/Coins.java
+++ Java/Coins.java
@@ -1,52 +1,42 @@
 import java.util.Scanner;
+
 public class Coins {
 	public static void main(String args[]) {
-		Scanner console = new Scanner(System.in);
-		System.out.println("This program converts cents into change.");
-		System.out.println("Please type the number of cents you want converted into change.");
-		System.out.println("Your number must be between 1 and 99.");
-		int cents = console.nextInt();
-		int numberOfQuarters = 0;
-		int numberOfDimes = 0;
-		int numberOfNickels = 0;
-		int numberOfPennies = 0;
+    String[] coinNames  = new String[] {"Toonie", "Loonie", "Quarter", "Dime", "Nickel", "Penny"};
+    int[] coinCount = new int[] {0, 0,   0,  0,   0,   0};
+    int[] centValue  = new int[] {200, 100, 25, 10, 5, 1};
 
-		do {
-			if(cents >= 25) {
-				numberOfQuarters++;
-				cents = cents - 25;
-			}
-			else if(cents >= 10) {
-				numberOfDimes++;
-				cents = cents - 10;
-			}
-			else if(cents >= 5) {
-				numberOfNickels++;
-				cents = cents - 5;
-			}
-			else {
-				numberOfPennies++;
-				cents = cents -1;
-			}
-		} while(cents > 0);
+    int dollars = inputAmount();
+    findChange(dollars, coinCount, centValue);
+    printChange(coinNames, coinCount);
+    }
 
-		do {
-			if(numberOfQuarters >= 1) {
-				System.out.println("Quarter");
-				numberOfQuarters--;
-			}
-			else if(numberOfDimes >= 1) {
-				System.out.println("Dime");
-				numberOfDimes--;
-			}
-			else if(numberOfNickels >= 1) {
-				System.out.println("Nickel");
-				numberOfNickels--;
-			}
-			else if(numberOfPennies >= 1) {
-				System.out.println("Penny");
-				numberOfPennies--;
-			}
-		} while(numberOfQuarters > 0 || numberOfDimes > 0 || numberOfNickels > 0 || numberOfPennies > 0);
-	}
+  public static int inputAmount() {
+    Scanner console = new Scanner(System.in);
+		System.out.println("This program converts a dollar amount into change.");
+		System.out.println("Please type the amount of money you want converted into change.");
+		System.out.print("$");
+		double inputAmount = console.nextDouble();
+    int dollars = (int)(inputAmount*100);
+    // Converting to int because 11 cents would result in 1 dime otherwise
+    return dollars;
+  }
+
+  public static void findChange(int dollarAmount, int[] numberOfCoins, int[] centValue){
+    int coinTally = 0;
+
+    for (int i = 0; i < numberOfCoins.length; i++){
+      while(dollarAmount >= centValue[i]) {
+        dollarAmount -= centValue[i];
+        numberOfCoins[i] = ++coinTally;
+      }
+      coinTally = 0;
+    }
+  }
+
+  public static void printChange(String[] coinNames, int[] numberOfCoins) {
+    for (int i = 0; i < numberOfCoins.length; i++){
+    System.out.println(numberOfCoins[i] + " " + coinNames[i]);
+    }
+  }
 }
